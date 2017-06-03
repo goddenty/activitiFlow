81764d04dd46b916e5ead901ccd8e820fbbe3acd
perkinsfang <fangcunlei@126.com>
2017-06-03 16:10:06 +0800

    99999999

--- activitiFlow/src/main/java/com/perkins/controller/TestController.java
+++ activitiFlow/src/main/java/com/perkins/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.perkins.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.perkins.service.IMailSendRecordService;
 
 @Controller
@@ -20,10 +24,25 @@ public class TestController {
 	   return "test1";
    }
    
+   @RequestMapping("/toTest2")
+   public String toTest2(){
+	   System.out.println(1112222222);
+	   return "test2";
+   }
+   
    @ResponseBody
    @RequestMapping("/test2")
-   public  String test2(){
-	   System.out.println(222222222);
-	   return "{'id':12345,'name':'jerry'}";
+   public  String test2(@RequestParam Map<String,Object> param ){
+	   System.out.println(222222222+","+param.get("content")+","+param.get("username"));
+	   ObjectMapper obj=new ObjectMapper();
+	   return "{id:12345,name:'jerry'}";
+   }
+   
+   @ResponseBody
+   @RequestMapping("/testRequest")
+   public  String testRequest(@RequestParam Map<String,Object> param ){
+	   System.out.println(222222222+","+param.get("content")+","+param.get("username"));
+	   ObjectMapper obj=new ObjectMapper();
+	   return "{id:12345,name:'jerry'}";
    }
 }
